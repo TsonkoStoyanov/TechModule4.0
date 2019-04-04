3ea5e4b9efd352597bc9cedba7ffd12f555a9f16
Tsonko <t_stoianov@yahoo.com>
2019-04-05 00:43:35 +0300

    Web CRUD

---
+++ 13.Basic CRUD/Basic CRUD-LAB/ToDoList - Skeleton/ToDoList/Data/ToDoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        private const string connectionString = @"Server=(localdb)\MSSQLLocalDB;DATABASE= ToDoListDb;Integrated Security=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
