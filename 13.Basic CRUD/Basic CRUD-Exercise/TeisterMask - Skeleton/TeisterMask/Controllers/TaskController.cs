4ed0f0ddf4d0dd2b07b86634730f1984790b0888
Tsonko <t_stoianov@yahoo.com>
2019-04-06 00:24:39 +0300

    More Basic CRUD

---
+++ 13.Basic CRUD/Basic CRUD-Exercise/TeisterMask - Skeleton/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TeisterMask.Data;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+
+                return View(tasks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string status)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                return RedirectToAction("Index");
+            }
+
+            Task task = new Task()
+            {
+                Title = title,
+                Status = status
+            };
+
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(taskToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Update(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                var taskToDelete = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (taskToDelete == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(taskToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 13.Basic CRUD/Basic CRUD-Exercise/TeisterMask - Skeleton/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeisterMask.Models;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        private const string connectionString = @"Server=(localdb)\MSSQLLocalDB;DATABASE=TeisterMaskDb;Integrated Security=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
